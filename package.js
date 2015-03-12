a5dce7d1b7fd1017f5253a84bb75833759087a2e
Miguel Angel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2015-03-12 21:40:04 +0100

    version 0.4.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'miguelalarcos:soop',
-  version: '0.4.4',
+  version: '0.4.5',
   summary: 'Simple Object Oriented Programming for Meteor',
   git: 'https://github.com/miguelalarcos/soop.git',
   documentation: 'README.md'
